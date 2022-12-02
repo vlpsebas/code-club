dc6352bfd01a6ee7b74fea82e732b616b2c37f7b
vlpsebas <vlada@cloudflare.com>
2022-12-02 14:51:19 +0000

    export default { 	fetch(request) { 		console.log(request.method) 		if(request.url == "https://clubhw.vsebas.workers.dev/" & request.method == "GET") { 			return new Response('Hello worker!', { 				headers: { 					'content-type': 'text/plain', 				}, 			}); 		} 		if(request.url == "https://clubhw.vsebas.workers.dev/" & request.method == "POST") { 			return new Response('why you change me?', { 				headers: { 					'content-type': 'application/json', 				}, 			}); 		} 		else{ 			return new Response('Error Worker!', { 				headers: { 					'content-type': 'text/plain', 				}, 			}); 		} 	},  };

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+	fetch(request) {
+		console.log(request.method)
+		if(request.url == "https://clubhw.vsebas.workers.dev/" & request.method == "GET") {
+			return new Response('Hello worker!', {
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
+		if(request.url == "https://clubhw.vsebas.workers.dev/" & request.method == "POST") {
+			return new Response('why you change me?', {
+				headers: {
+					'content-type': 'application/json',
+				},
+			});
+		}
+		else{
+			return new Response('Error Worker!', {
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
 	},
-};
+ };
